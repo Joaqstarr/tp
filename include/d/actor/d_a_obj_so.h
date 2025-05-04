ad7c9ed170f9557dfee4c956302ab635ffd17e70
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-05-04 12:03:31 -0400

    Work setting up struct

--- include/d/actor/d_a_obj_so.h
+++ include/d/actor/d_a_obj_so.h
@@ -2,6 +2,11 @@
 #define D_A_OBJ_SO_H
 
 #include "f_op/f_op_actor_mng.h"
+#include "SSystem/SComponent/c_phase.h"
+
+
+class J3DModel;
+
 
 /**
  * @ingroup actors-objects
@@ -12,9 +17,24 @@
  *
  */
 class obj_so_class : public fopAc_ac_c {
-private:
-    /* 0x568 */ u8 field_0x568[0x1bd0 - 0x568];
+
+public:
+    /* 0x568 */ request_of_phase_process_class mPhase;
+    /* 0x570 */ u8 field_0x570;
+    /* 0x571 */ u8 field_0x571;
+    /* 0x572 */ u8 field_0x572[2];
+	/* 0x574 */ J3DModel* mpModel;
+    /* 0x578 */ u8 field_0x578[28];
+    /* 0x594 */ J3DModel* mpPartModels[26];
+    /* 0x5fc */ u8 mPartDrawFlags[26];
+    /* 0x616 */ u8 field_0x616[2];
+	/* 0x618 */ cXyz mPartWorldPositions[26];
+    /* 0x750 */ u8 field_0x750[105];
+	/* 0x7b8 */ cXyz mPartScale[26];
+    /* 0x8f0 */ cXyz mPartRandPos[26];
+
 };
+
 
 STATIC_ASSERT(sizeof(obj_so_class) == 0x1bd0);
 
